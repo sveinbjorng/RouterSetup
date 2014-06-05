0115f8e2378cdc43661432eeb8669a538da63d5d
Sveinbjörn Guðjohnsen <sveinbjorn.marion@gmail.com>
2014-06-05 17:19:55 +0000

    Linksys E900 is now working correctly

--- src/Linksys.py
+++ src/Linksys.py
@@ -49,9 +49,9 @@ class E900:
             urllib2.install_opener(self.opener)
             
             self.opener.open(self.unsecuredUrl, params)
-            
+            print "First time setup: Done!"
         except Exception, e:
-            print str(e)
+            print "First time setup: " + str(e) + " (This is normal if router has been setup before)"
         
     def LoginToRouter(self):
         if(self.opener == None):
@@ -72,9 +72,12 @@ class E900:
                 p = re.compile(ur'\;(.*?)\"')
                 p = re.findall(p, l)
                 self.sessionId = ''.join(p)
-    def ConfigureWireless(self, ssid, wirelessKey):
+        print "Login: Done!"
+                
+    def ConfigureSSID(self, ssid, channel):
         headers = [('Referer','http://192.168.1.1/Wireless_Basic.asp;' + self.sessionId)]
         self.opener.addheaders = headers
+        
         params = urllib.urlencode(dict({
             "submit_button":"Wireless_Basic",
             "gui_action":"Apply",
@@ -83,7 +86,7 @@ class E900:
             "next_page":"",
             "commit":"1",
             "wl0_nctrlsb":"upper",
-            "channel_24g":"11",
+            "channel_24g":channel,
             "nbw_24g":"20",
             "wait_time":"3",
             "guest_ssid":"",
@@ -95,9 +98,74 @@ class E900:
             "_wl0_channel":"11",
             "closed_24g":"0"}))
         self.opener.open(self.applyUrl+self.sessionId, params)
-            
+        print "Wireless: SSID configured!"
+        
+    def ConfigureWirelessKey(self, wirelessKey):    
+        headers = [('Referer', 'http://192.168.1.1/WL_WPATable.asp;' + self.sessionId)]
+        self.opener.addheaders = headers
+
+        params = urllib.urlencode(dict({
+            "submit_button":"WL_WPATable",
+            "change_action":"",
+            "submit_type":"",
+            "gui_action":"Apply",
+            "security_mode_last":"",
+            "wl_wep_last":"",
+            "wait_time":"3",
+            "wsc_nwkey0":wirelessKey,
+            "wl0_crypto":"tkip+aes",
+            "wsc_security_auto":"0",
+            "wl0_security_mode":"wpa_wpa2_mixed",
+            "wl0_wpa_psk":wirelessKey                    
+        }))
+        self.opener.open(self.applyUrl+self.sessionId, params)
+        print "Wireless: Wireless security configured!"
+        print "Wireless: Done!"
+    
+    def DisableWMMSupport(self):
+        headers = [('Referer', 'http://192.168.1.1/QoS.asp;' + self.sessionId)]
+        self.opener.addheaders = headers
+        
+        params = urllib.urlencode(dict({
+            "qos_list":"",
+            "QoS_cnt":"0",
+            "enable_game":"0",
+            "submit_button":"QoS",
+            "change_action":"",
+            "gui_action":"Apply",
+            "need_action":"restart",
+            "wait_time":"30",
+            "wl_wme":"off",
+            "QoS":"0",
+            "fport1":"",
+            "tport1":"",
+            "pro1":"0",
+            "pro2":"0",
+            "pro3":"0"}))
+        
+        self.opener.open(self.applyUrl+self.sessionId, params)
+        print "WMM Support: Done!"
+        
 if __name__ == '__main__':
-    router = E900()
-    #router.DealWithUnsecure()
-    router.LoginToRouter()
-    router.ConfigureWireless("SveimbaSSID", "SveimbaKey")
+    ssid = raw_input("Enter SSID: ")
+    wirelessKey = raw_input("Enter wireless key: ")
+    channel = raw_input("Enter wireless channel: ")
+    
+    print "SSID: " + ssid
+    print "Wireless key: " + wirelessKey
+    print "Channel: " + channel
+    yn = raw_input("Continue? (y/n)")
+    
+    if(yn == 'y'):
+        router = E900()
+        print "First time setup: Dealing with unsecure.."
+        router.DealWithUnsecure()
+        print "Login: Trying to login.."
+        router.LoginToRouter()
+        print "Wireless: Trying to setup wireless.."
+        router.ConfigureSSID(ssid, channel)
+        router.ConfigureWirelessKey(wirelessKey)
+        print "WMM Support: Trying to disable.."
+        router.DisableWMMSupport()
+    print "Router setup is complete!"
+    
